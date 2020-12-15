e9e37c806a93e51a40fa5b5d2028ff8238720580
AttributeErrorStuff <71349701+AttributeErrorStuff@users.noreply.github.com>
2020-12-15 21:32:32 +0200

    Update NavBar.js

--- docs/NavBar.js
+++ docs/NavBar.js
@@ -3,13 +3,13 @@ let forum = document.getElementById("forum");
 let other = document.getElementById("other");
 
 home.onclick = function() {
-    window.location.replace('https://www.stopracism.ga/')
+    window.location.replace('www.stopracism.ga/')
 }
 
 forum.onclick = function() {
-    window.location.replace('https://www.stopracism.ga/')
+    window.location.replace('www.stopracism.ga/')
 }
 
 other.onclick = function() {
-    window.location.replace('https://www.stopracism.ga/')
+    window.location.replace('www.stopracism.ga/')
 }
